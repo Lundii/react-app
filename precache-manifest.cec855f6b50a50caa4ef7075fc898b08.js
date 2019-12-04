f6a52ce3d5e578c8c967024f167cf3c90930efa8
Onu Monday Emmanuel <mondayemmanuel67@gmail.com>
2019-12-04 23:28:42 +0100

    Updates

---
+++ precache-manifest.cec855f6b50a50caa4ef7075fc898b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e282fb329cf260b54a27e33ac0baf6a2",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "2e4a350271a48c865671",
+    "url": "/react-app/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "a351abbc7e45b837967f",
+    "url": "/react-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "2e4a350271a48c865671",
+    "url": "/react-app/static/js/2.6b85907b.chunk.js"
+  },
+  {
+    "revision": "a351abbc7e45b837967f",
+    "url": "/react-app/static/js/main.1fecafbc.chunk.js"
+  },
+  {
+    "revision": "9b417d75f9f42c249c23",
+    "url": "/react-app/static/js/runtime-main.5f0f8821.js"
+  }
+]);
